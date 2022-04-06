98d6804e5e6bea8b01dd2d4d18032a8d83038d00
IamGroot2001 <daohoanh01637788488@gmail.com>
2022-04-06 10:45:03 +0700

    them hello

--- hello/src/hello/Hello.java
+++ hello/src/hello/Hello.java
@@ -17,6 +17,7 @@ public class Hello {
         // TODO code application logic here
         System.out.println("Dao Ho Anh");
         System.out.println("Ho Dang Dung");
+        System.out.println("hello.Hello.main()");
     }
     
 }
